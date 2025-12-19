a961cb99ac7e2943dc561a00cde446af70c186ea
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2025-12-18 18:25:11 -0800

    auth controllers

---
+++ src/controllers/authController.ts
@@ -0,0 +1,89 @@
+import type { Request, Response } from "express";
+import { db } from '../db/connection.ts'
+import { users, type NewUser } from "../db/schema.ts";
+import { generateToken } from "../utils/jwt.ts";
+import { comparePasswords, hashPassword } from "../utils/passwords.ts";
+import { eq } from "drizzle-orm";
+
+
+export const register = async (
+    req: Request <any, any, NewUser>, 
+    res: Response
+) => {
+    try {
+        const hashedPassword = await hashPassword(req.body.password)
+
+        const [user] = await db
+            .insert(users)
+            .values({
+                ...req.body,
+                password: hashedPassword,
+            })
+            .returning({
+                id: users.id,
+                email: users.email,
+                username: users.username,
+                firstName: users.firstName,
+                lastName: users.lastName,
+                createdAt: users.createdAt,
+            })
+        
+        const token = await generateToken({
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        })
+
+        return res.status(201).json({
+            message: 'User Created',
+            user,
+            token,
+        })
+    } catch (e) {
+        console.error(`Registration error`, e)
+        res.status(500).json({ error : `Failed to create user`})
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body
+        const user = await db.query.users.findFirst({
+            where: eq(users.email, email)
+        })
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid Credentials'})
+        }
+        
+        const isValidPassword = comparePasswords(password, user.password)
+
+        if (!isValidPassword) {
+            return res.status(401).json({ error: 'Invalid Credentials' })
+        }
+
+        const token = await generateToken({
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        })
+
+        return res
+            .json({
+                message: 'Login Success',
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    username: user.username,
+                    firstName: user.firstName,
+                    lastname: user.lastName,
+                    createdAt: user.createdAt,
+                },
+                token,
+            })
+            .status(201)
+    } catch (e) {
+        console.error('Login Error', e)
+        res.status(500).json({ error: 'Failed to Login' })
+    }
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import { validateBody } from "../middleware/validation.ts";
+import { insertUserSchema } from "../db/schema.ts";
+import { login, register } from "../controllers/authController.ts";
+import {z} from 'zod'
+
+const loginSchema = z.object({
+    email: z.email('Invalid Email'),
+    password: z.string().min(1, 'Password Required')
+})
 
 const router = Router()
 
-router.post("/register", (req, res) => {
-    res.status(201).json({ message: 'User signed up'})
-})
+router.post("/register", validateBody(insertUserSchema), register) 
 
-router.post("/login", (req, res) => {
-    res.status(201).json({ message: 'User signed in'})
-})
+router.post("/login", validateBody(loginSchema), login)
 
-export default router
+export default router 
